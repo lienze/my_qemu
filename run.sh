816de9dc44bd6e23dae50b80cd5cd0c36b835139
Enze Li <lienze2010@126.com>
2020-03-13 18:16:29 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,25 @@
-qemu-system-x86_64 -s -kernel ../linux-4.19.108/arch/x86/boot/bzImage \
-	-initrd initramfs.cpio.gz \
-	-nographic \
-	-cpu max \
-	-append "console=ttyS0"
+#!/bin/bash
+if [ x$1 == x"kvm" ];then
+	qemu-system-x86_64 \
+		-s \
+		-kernel ../linux-4.19.108/arch/x86/boot/bzImage \
+		-initrd initramfs.cpio.gz \
+		-nographic \
+		-cpu host \
+		-enable-kvm \
+		-smp 2 \
+		-m 2048 \
+		-append "console=ttyS0"
+elif [ x$1 == x"4" ];then
+	qemu-system-x86_64 \
+		-s \
+		-kernel ../linux-4.19.108/arch/x86/boot/bzImage \
+		-initrd initramfs.cpio.gz \
+		-nographic \
+		-cpu max \
+		-smp 2 \
+		-m 2048 \
+		-append "console=ttyS0"
+else
+	echo "Hello World"
+fi
