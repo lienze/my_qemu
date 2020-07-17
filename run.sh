798698395407dfce19cd3451662506fb66b0d0d2
Enze Li <lienze2010@126.com>
2020-07-17 14:26:59 +0800

    added a new para
    Signed-off-by: Enze Li <lienze2010@126.com>

--- run.sh
+++ run.sh
@@ -20,6 +20,16 @@ elif [ x$1 == x"4" ];then
 		-smp 2 \
 		-m 2048 \
 		-append "console=ttyS0"
+elif [ x$1 == x"5" ];then
+	qemu-system-x86_64 \
+		-s \
+		-kernel ../linux_trunk/arch/x86/boot/bzImage \
+		-initrd initramfs.cpio.gz \
+		-nographic \
+		-cpu max \
+		-smp 2 \
+		-m 2048 \
+		-append "console=ttyS0"
 else
 	echo "use kvm, 4, 5 para to start..."
 fi
