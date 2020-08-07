84897489519bfa93159e6c75f88407d007069241
Enze Li <lienze2010@126.com>
2020-08-07 11:37:24 +0800

    added new function

--- pack.sh
+++ pack.sh
@@ -1,4 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+this_dir=`pwd`
+
+function delete_dir()
+{
+	if [ -d $1 ];then
+		rm -rf $1
+		echo "delete $1 succeed"
+	fi
+}
+
 cd ./rootfs
+delete_dir ./dev
+mkdir ./dev
+sudo cp -a /dev/{null,console,tty,tty1,tty2,tty3,tty4} ./dev
 find ./ -print0 | cpio --null -ov --format=newc | gzip -9 > ../rootfs.cpio.gz
 
