3ad16ad8e7d1ac797946edd227b101c87d26b86b
Enze Li <lienze2010@126.com>
2020-08-11 10:43:24 +0800

    added proc and sys folders when packing the rootfs
    Signed-off-by: Enze Li <lienze2010@126.com>

--- pack.sh
+++ pack.sh
@@ -9,13 +9,23 @@ function delete_dir()
 	fi
 }
 
-delete_dir ./rootfs/lib/modules
-mkdir -p ./rootfs/lib/modules
-cp ../ubuntu_src/drivers/atfs/atfs.ko ./rootfs/lib/modules
+function make_dir()
+{
+	if [ -d $1 ];then
+		rm -rf $1
+		echo "delete $1 succeed"
+	fi
+	mkdir -p $1
+}
+
+make_dir ./rootfs/proc
+make_dir ./rootfs/sys
+
+make_dir ./rootfs/lib/modules
+cp ../debian/drivers/atfs/atfs.ko ./rootfs/lib/modules
 
 cd ./rootfs
-delete_dir ./dev
-mkdir ./dev
+make_dir ./dev
 sudo cp -a /dev/{null,console,tty,tty1,tty2,tty3,tty4} ./dev
 find ./ -print0 | cpio --null -ov --format=newc | gzip -9 > ../rootfs.cpio.gz
 
